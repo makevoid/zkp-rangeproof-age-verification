feb9be32014ba007e4441d7722d11db9c3364383
arturo.manzaneda <arturo.manzaneda.tudela@ing.com>
2018-12-13 16:40:21 +0100

    Add nav bar config

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/zkproofs/'
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    base: '/zkproofs/',
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: 'Github', link: 'https://github.com/ing-bank/zkproofs'},
+        ]
+    }
 };
--- docs/docs/README.md
+++ docs/docs/README.md
@@ -1,1 +1,11 @@
-# Hello VuePress
+---
+home: true
+heroImage: /ing-logo.png
+heroText: Zero Knowledge Proof
+tagline: ING's implementation of ZKP
+actionText: Get Started →
+actionLink: /guide/
+footer: LGPL-3.0 Licensed | ING Bank
+---
+
+# Zero Knowledge Proof
